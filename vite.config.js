47de168daf6eca8b9fd0c2634fdb61e799c7abdc
Ivan Sotnikov <v.sotnikov95@mail.ru>
2022-05-06 14:46:32 +0300

    build ver

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Gallery_on_VUE3/',
+  base:'/',
   plugins: [vue()],
   resolve: {
     alias: {
